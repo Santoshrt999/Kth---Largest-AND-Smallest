f192662f71898c0acf26a53b9a3999197465c315
Santosh Ravi Teja <S528130@MAIL.NWMISSOURI.EDU>
2021-02-28 09:50:45 -0500

    Update KthLargest.java

--- KthLargest.java
+++ KthLargest.java
@@ -1,5 +1,59 @@
 package com.graphs;
 
+/**
+ * 
+ * @author Santosh Goteti
+ *
+ */
 public class KthLargest {
+
+	public int findKthLargest(int[] nums, int k) {
+
+
+		//Code for Quick Select Algorithm
+
+		int high = nums.length-1;
+		int n= nums.length-k;
+		int low=0;
+
+		while(low < high){
+			int mid = partition(nums, low, high);
+
+			if(n > mid){
+				low = mid + 1;
+			}
+			else if (n < mid){
+				high = mid - 1;
+			}
+			else 
+				return nums[mid];
+		}
+
+		return nums[high];
+	}
+
+	int partition(int[] arr, int low, int high){
+
+		int pivot=arr[high];
+		int i=low;
+
+		for(int j=low; j<arr.length; j++){
+			if(arr[j] < pivot){
+
+				int temp = arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				i++;
+			}
+		}
+
+
+		int temp = arr[i];
+		arr[i]=arr[high];
+		arr[high]=temp;
+
+		return i;
+
+	}
 
 }
